1d5b7f54ee7c1a797e05d3f894b8565711b6c218
div-antk <51283468+owaii@users.noreply.github.com>
2023-01-01 23:59:00 +0900

    Taskエンティティの追加

--- app/src/main/java/com/example/roomtodo/MainActivity.kt
+++ app/src/main/java/com/example/roomtodo/MainActivity.kt
@@ -17,11 +17,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RoomTodoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 )
+                {
+
+                }
             }
         }
     }
