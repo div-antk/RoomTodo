ee19736c973cf5f10791c44d4d9affc1ec5ad166
div-antk <51283468+owaii@users.noreply.github.com>
2023-01-07 23:53:19 +0900

    誤った記述を修正、追記

--- app/src/main/java/com/example/roomtodo/TaskDao.kt
+++ app/src/main/java/com/example/roomtodo/TaskDao.kt
@@ -5,7 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
-import java.util.concurrent.Flow
+import kotlinx.coroutines.flow.Flow
 
 class TaskDao {
     @Dao
