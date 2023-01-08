5255e00f0095b2f10a2da578341c35f76788b0c0
div-antk <51283468+owaii@users.noreply.github.com>
2023-01-08 23:59:00 +0900

    ビルドエラーの解消、Hiltのアプリケーションクラスの実装

--- app/src/main/java/com/example/roomtodo/MainActivity.kt
+++ app/src/main/java/com/example/roomtodo/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.roomtodo.ui.theme.RoomTodoTheme
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
