06976af8c58e8643600a864624dca1df0d69bad5
div-antk <51283468+owaii@users.noreply.github.com>
2023-01-09 00:28:07 +0900

    Hiltモジュールの作成

--- app/src/main/java/com/example/roomtodo/MainActivity.kt
+++ app/src/main/java/com/example/roomtodo/MainActivity.kt
@@ -11,9 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.roomtodo.ui.theme.RoomTodoTheme
-import dagger.hilt.android.HiltAndroidApp
+//import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+//@HiltAndroidApp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
